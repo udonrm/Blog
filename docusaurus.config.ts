c25849d2be57e9efb1f3091b7f423e1b873fe706
udonrm <b201395@hiroshima-u.ac.jp>
2025-03-19 17:23:25 +0900

    GitHub Actionsを設定

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,15 +9,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://udonrm.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/TechTechUdon',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'udonrm', // Usually your GitHub org/user name.
-  projectName: 'TechTechUdon', // Usually your repo name.
+  projectName: 'Blog', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
